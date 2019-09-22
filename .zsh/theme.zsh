232720a1ff17e8fe62632daa7e76e868054da378
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-09-23 00:22:57 +0900

    Set git user color

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -2,6 +2,7 @@ agnoster_theme_display_git_master_branch=1
 agnoster_theme_display_status_success=1
 agnoster_theme_newline_cursor=1
 agnoster_theme_color_status_bg=15
+agnoster_theme_color_git_user=75
 
 zplugin snippet OMZ::plugins/shrink-path/shrink-path.plugin.zsh
 zplugin snippet "https://github.com/Ryooooooga/agnoster-zsh-theme/blob/master/agnoster.zsh-theme"
