39d0a415663e030f04c65c84d58ced89b28fbf72
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-10-13 08:46:56 +0900

    Set asdf-vm data directory

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -133,11 +133,11 @@ if [ -e "$ANYENV_ROOT" ]; then
 fi
 
 ### asdf-vm ###
-export ASDF_DIR="$XDG_DATA_HOME/asdf"
+export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 
-if [ -e "$ASDF_DIR" ]; then
-	source $ASDF_DIR/asdf.sh
-	source $ASDF_DIR/completions/asdf.bash
+if [ -e "$ASDF_DATA_DIR" ]; then
+	source $ASDF_DATA_DIR/asdf.sh
+	source $ASDF_DATA_DIR/completions/asdf.bash
 fi
 
 ### local ###
