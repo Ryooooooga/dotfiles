4dcb7396c0a9e10c3edaeff252f0ca0c57f78a9a
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-10-13 09:55:07 +0900

    Update npm cache dir

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,6 +1,7 @@
 ### XDG ###
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 ### zplugin ###
 typeset -gAH ZPLGM
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -155,6 +155,7 @@ fi
 ### npm ###
 export NPM_CONFIG_DIR="$XDG_CONFIG_HOME/npm"
 export NPM_DATA_DIR="$XDG_DATA_HOME/npm"
+export NPM_CACHE_DIR="$XDG_CACHE_HOME/npm"
 export NPM_CONFIG_USERCONFIG="$NPM_CONFIG_DIR/npmrc"
 
 ### local ###
