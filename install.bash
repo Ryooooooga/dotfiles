f1307eaf4b19ec25b62f2670f68d81514450e735
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-11-02 10:08:01 +0900

    Imrpove scripts with shellcheck

--- install.bash
+++ install.bash
@@ -12,7 +12,7 @@ function clone() {
 	fi
 }
 
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || return 1
 XDG_CONFIG_HOME="$HOME/.config"
 XDG_DATA_HOME="$HOME/.local/share"
 
@@ -23,7 +23,7 @@ XDG_DATA_HOME="$HOME/.local/share"
 clone https://github.com/zdharma/zplugin "$XDG_DATA_HOME/zplugin/bin"
 
 # dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s $XDG_DATA_HOME/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s "$XDG_DATA_HOME/dein"
 
 # asdf-vm
 clone https://github.com/asdf-vm/asdf "$XDG_DATA_HOME/asdf"
--- symlink.bash
+++ symlink.bash
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || return 1
 
 mkdir -p ~/.config/
-ln -sf $PWD/gitconfig        ~/.gitconfig
-ln -sf $PWD/zshenv           ~/.zshenv
-ln -sf $PWD/vimrc            ~/.vimrc
-ln -sf $PWD/config/zsh       ~/.config/
-ln -sf $PWD/config/nvim      ~/.config/
-ln -sf $PWD/config/git       ~/.config/
-ln -sf $PWD/config/tmux      ~/.config/
-ln -sf $PWD/config/npm       ~/.config/
-ln -sf $PWD/config/alacritty ~/.config/
+ln -sf "$PWD/gitconfig"        ~/.gitconfig
+ln -sf "$PWD/zshenv"           ~/.zshenv
+ln -sf "$PWD/vimrc"            ~/.vimrc
+ln -sf "$PWD/config/zsh"       ~/.config/
+ln -sf "$PWD/config/nvim"      ~/.config/
+ln -sf "$PWD/config/git"       ~/.config/
+ln -sf "$PWD/config/tmux"      ~/.config/
+ln -sf "$PWD/config/npm"       ~/.config/
+ln -sf "$PWD/config/alacritty" ~/.config/
 
 mkdir -p ~/.cache/zsh
 
 if [ "$(uname)" = "Darwin" ]; then
 	touch ~/.hushlogin
-	ln -sf $PWD/config/hammerspoon ~/.config/
-	ln -sf $PWD/config/karabiner   ~/.config/
+	ln -sf "$PWD/config/hammerspoon" ~/.config/
+	ln -sf "$PWD/config/karabiner"   ~/.config/
 fi
