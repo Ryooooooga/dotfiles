e59896eeb240866759ce29480aeec1f5e7ff97f7
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-12-11 19:51:12 +0900

    zsh: Unset GPG home

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -134,9 +134,6 @@ LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:
 ### less ###
 export LESSHISTFILE=-
 
-### GPG ###
-export GNUPGHOME="$XDG_DATA_HOME/gnupg"
-
 ### completion styles ###
 zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
