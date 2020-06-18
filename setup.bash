53903f7939e367535561e4328b554baab8a824bb
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-06-18 11:30:33 +0900

    update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,45 +3,49 @@ REPO_DIR="$(cd "$(dirname "$0")" || exit 1; pwd)"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
-clone() {
-    local repo
-    local dest
-    repo="$1"
-    dest="$2"
-
-    if [ ! -e "$dest" ]; then
-        git clone "$repo" "$dest"
-    else
-        echo "$dest is already exists..."
-        git -C "$dest" pull
-    fi
-}
-
-github() {
-    clone "https://github.com/$1" "$2"
-}
-
 "$REPO_DIR"/link.bash
 
 # zplugin
-github zdharma/zinit "$XDG_DATA_HOME/zinit/bin"
+echo "Installing zinit..."
+if [ -d "$XDG_DATA_HOME/zinit/bin" ]; then
+    echo "zinit is already installed."
+    git -C "$XDG_DATA_HOME/zinit/bin" pull
+else
+    git clone "https://github.com/zdharma/zinit" "$XDG_DATA_HOME/zinit/bin"
+fi
 
 # dein.vim
-if [ ! -d "$XDG_DATA_HOME/dein/repos/github.com/Shougo/dein.vim" ]; then
-    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s "$XDG_DATA_HOME/dein"
-else
-    echo "$XDG_DATA_HOME/dein is already exists..."
+echo "Installing dein.vim..."
+if [ -d "$XDG_DATA_HOME/dein/repos/github.com/Shougo/dein.vim" ]; then
+    echo "dein.vim is already installed."
     git -C "$XDG_DATA_HOME/dein/repos/github.com/Shougo/dein.vim" pull
+else
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s "$XDG_DATA_HOME/dein"
 fi
 
 # asdf-vm
-github asdf-vm/asdf "$XDG_DATA_HOME/asdf"
+echo "Installing asdf-vm..."
+if [ -d "$XDG_DATA_HOME/asdf" ]; then
+    echo "asdf-vm is already installed."
+    git -C "$XDG_DATA_HOME/asdf" pull
+else
+    git clone "https://github.com/asdf-vm/asdf" "$XDG_DATA_HOME/asdf"
+fi
 
 # gdb-dashboard
-github cyrus-and/gdb-dashboard "$XDG_DATA_HOME/gdb-dashboard"
+echo "Installing gdb-dashboard..."
+if [ -d "$XDG_DATA_HOME/gdb-dashboard" ]; then
+    echo "gdb-dashboard is already installed."
+    git -C "$XDG_DATA_HOME/gdb-dashboard" pull
+else
+    git clone "https://github.com/cyrus-and/gdb-dashboard" "$XDG_DATA_HOME/gdb-dashboard"
+fi
 
 # mac
 if [ "$(uname)" = "Darwin" ]; then
+    # Display hidden files in Finder
     defaults write com.apple.finder AppleShowAllFiles YES
+
+    # Configure hammerspoon config location
     defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
 fi
