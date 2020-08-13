f2d9b747676039715754250e5bcd4eef99e7ffec
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-08-13 10:18:27 +0900

    ubuntu: refine apt/install.sh

--- config/apt/install.sh
+++ config/apt/install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 ubuntu_version="$(lsb_release -r | awk '{print $2 * 100}')"
 
-curl -fsSL 'https://download.docker.com/linux/ubuntu/gpg' | apt-key add -
-
 add-apt-repository -y ppa:git-core/ppa &&
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
 apt-get update && apt-get upgrade -y &&
 apt-get install -y \
     autoconf \
@@ -12,8 +9,6 @@ apt-get install -y \
     build-essential \
     cmake \
     colordiff \
-    docker-ce \
-    docker-ce-cli \
     git \
     gpg \
     jq \
@@ -25,8 +20,26 @@ apt-get install -y \
     unzip \
     wget \
     zip \
-    zsh &&
+    zsh
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 if [ "$ubuntu_version" -ge 2004 ]; then
     # Ubuntu 20.04 or later
     apt-get install -y fd-find
+
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+
+curl -fsSL 'https://download.docker.com/linux/ubuntu/gpg' | apt-key add - &&
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+apt-get update &&
+apt-get install -y docker-ce docker-ce-cli
+
+if [ $? -ne 0 ]; then
+    exit 1
 fi
