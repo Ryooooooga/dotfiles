0727c6e79ddd449bc1c97cba501f8618ad7cb09e
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-08-16 15:03:50 +0900

    subl: fix link.bash

--- link.bash
+++ link.bash
@@ -50,5 +50,4 @@ if [ "$(uname)" = "Darwin" ]; then
     mkdir -p "$SUBL_DIR/Packages/User"
     ln -sfv "$XDG_CONFIG_HOME/sublime-text/User/"*.sublime-settings "$SUBL_DIR/Packages/User"
     ln -sfv "$XDG_CONFIG_HOME/sublime-text/User/"*.sublime-keymap   "$SUBL_DIR/Packages/User"
-    ln -sfv "$XDG_CONFIG_HOME/sublime-text/GHQ"                     "$SUBL_DIR/Packages"
 fi
