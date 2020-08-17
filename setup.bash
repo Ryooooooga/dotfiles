487fd150bee0db1441b17ef09c22ea6c887b2d6c
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-08-17 19:20:42 +0900

    mac: update homebrew before install apps

--- setup.bash
+++ setup.bash
@@ -35,6 +35,9 @@ if [ "$(uname)" = "Darwin" ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
+    echo "Updating Homebrew..."
+    brew update
+
     echo "Installing Homebrew apps..."
     brew bundle install --file "${REPO_DIR}/config/homebrew/Brewfile" --no-lock
 fi
