8b8dfe331e696b9fcc2130c1a3b298317a876cb7
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-09-08 21:05:03 +0900

    update link.bash

--- link.bash
+++ link.bash
@@ -4,7 +4,6 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 mkdir -p "$XDG_CONFIG_HOME"
-mkdir -p "$XDG_DATA_HOME/zsh"
 mkdir -p "$HOME/.gnupg"
 chmod 700 "$HOME/.gnupg"
 
