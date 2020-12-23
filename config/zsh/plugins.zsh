d7d9a6cae27e55cd06a499574b077044e13e28fe
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-12-23 17:10:46 +0900

    zsh: install `pmy`

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -140,6 +140,18 @@ zinit ice lucid wait"0" as"program" from"gh-r" \
     pick"ghq*/ghq"
 zinit light 'x-motemen/ghq'
 
+### pmy ###
+zinit ice lucid wait"0" as"program" from"gh-r" \
+    pick"pmy*/pmy" \
+    atload'
+        export PMY_TRIGGER_KEY="^P"
+        export PMY_RULE_PATH="$XDG_CONFIG_HOME/pmy/rules"
+        export PMY_SNIPPET_PATH="$XDG_CONFIG_HOME/pmy/snippets"
+        export PMY_LOG_PATH="$XDG_CACHE_HOME/pmy/log.txt"
+        eval "$(pmy init)"
+    '
+zinit light 'relastle/pmy'
+
 ### Emojify ###
 zinit ice lucid wait"0" as"program" \
     atclone'rm *.{py,bats}' atpull'%atclone'
