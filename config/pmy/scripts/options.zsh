8ebc1db9d85310ebaebbcf50ff8f943632bbc5f7
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-12-25 22:26:28 +0900

    zsh(pmy): add docker completion

--- config/pmy/scripts/options.zsh
+++ config/pmy/scripts/options.zsh
@@ -1,15 +1,18 @@
 #!/usr/bin/env zsh
+# options <snippet> <icon> <header> <color>
 . "${0:a:h}/ansi.zsh"
 
-icon="$(print "\ufb32")"
-header="command"
+snippet="$1"
+icon="$(print "$2")"
+header="$3"
+color="$4"
 
 reset="$ansi[reset]"
-header_style="$ansi[yellow]"
+header_style="$ansi[$color]"
 name_style="$ansi[bold]"
 desc_style="\x1b[38;5;65m"
 
-command cat "${0:a:h}/../snippets/git-subcommands.txt" \
+command cat "${0:a:h}/../snippets/$snippet.txt" \
     | awk -F '\t' "{
         printf \"$header_style$icon %-7s$reset  \", \"$header\"
         printf \"$name_style%-20s$reset  \", \$1
