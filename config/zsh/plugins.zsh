b3e47792da63a3703d23dfaee6531ced02f4738c
Ryooooooga <ryoga_314@yahoo.co.jp>
2020-12-26 19:41:18 +0900

    zsh(pmy): fzf with `--tiebreak=length` is broken

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -150,7 +150,7 @@ zinit ice lucid wait"0" as"program" from"gh-r" \
         export PMY_SNIPPET_PATH="$PMY_CONFIG_HOME/snippets"
         export PMY_LOG_PATH="$XDG_CACHE_HOME/pmy/log.txt"
         export PMY_SCRIPT_PATH="$PMY_CONFIG_HOME/scripts"
-        export PMY_FUZZY_FINDER_DEFAULT_CMD="fzf --exit-0 --select-1 --height=40% --cycle --preview-window=right:50%"
+        export PMY_FUZZY_FINDER_DEFAULT_CMD="fzf --exit-0 --select-1 --tiebreak='begin,index' --height=40% --cycle --preview-window=right:50%"
         eval "$(pmy init)"
     '
 zinit light 'relastle/pmy'
