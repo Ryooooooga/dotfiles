06cdda935eb5ad91635c6ea8c39f8e93b2c6874c
Ryooooooga <eial5q265e5@gmail.com>
2021-05-02 20:07:44 +0900

    zouch: add go templates

---
+++ config/zouch/templates/_.go
@@ -0,0 +1,12 @@
+{{ if HasSuffix .Filename "_test.go" -}}
+package {{.Filename | Abs | Dir | Base}}_test
+
+import (
+	"testing"
+)
+
+func Test{{RegexReplaceAll .Filename `_test.go$` "" | Base | UpperCamelCase}}(t *testing.T) {
+}
+{{ else -}}
+package {{.Filename | Abs | Dir | Base}}
+{{ end -}}
