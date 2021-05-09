fa98838fbbee19ac2fb2336bcdc6a1a0a51e13a7
Ryooooooga <eial5q265e5@gmail.com>
2021-05-09 21:54:07 +0900

    cmake: add llvm.cmake

---
+++ config/cmake/llvm.cmake
@@ -0,0 +1,14 @@
+find_package(LLVM REQUIRED CONFIG)
+
+llvm_map_components_to_libnames(llvm_libs
+    core
+    support
+    analysis
+    executionengine
+    interpreter
+)
+
+add_library(llvm INTERFACE)
+target_include_directories(llvm INTERFACE ${LLVM_INCLUDE_DIRS})
+target_link_libraries(llvm INTERFACE ${llvm_libs})
+target_compile_definitions(llvm INTERFACE ${LLVM_DEFINITIONS})
