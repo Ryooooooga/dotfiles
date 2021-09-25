048c791270ad2b2909ed26db2202263950349372
Ryooooooga <eial5q265e5@gmail.com>
2021-09-26 01:59:41 +0900

    cmake: add cuutest.cmake

---
+++ config/cmake/cuutest.cmake
@@ -0,0 +1,6 @@
+FetchContent_Declare(cuutest
+    GIT_REPOSITORY "https://github.com/Ryooooooga/cuutest.git"
+    GIT_TAG        "main"
+)
+
+FetchContent_MakeAvailable(cuutest)
---
+++ config/zouch/templates/cuutest.cmake
@@ -0,0 +1,1 @@
+../../cmake/cuutest.cmake
