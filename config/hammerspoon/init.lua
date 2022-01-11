6e67b828fa41b0486a677a9eb5600be23a56b618
Ryooooooga <eial5q265e5@gmail.com>
2022-01-12 00:31:25 +0900

    editorconfig: update lua settings

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -2,19 +2,19 @@ hs.alert.show('Config loaded')
 
 -- Reload
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'R', function()
-    hs.reload()
+  hs.reload()
 end)
 
 local function keyStroke(mod, key)
-    return function() hs.eventtap.keyStroke(mod, key, 1000) end
+  return function() hs.eventtap.keyStroke(mod, key, 1000) end
 end
 
 local function keyStrokes(text)
-    return function() hs.eventtap.keyStrokes(text) end
+  return function() hs.eventtap.keyStrokes(text) end
 end
 
 local function remap(mod, key, stroke)
-    hs.hotkey.bind(mod, key, stroke, nil, stroke)
+  hs.hotkey.bind(mod, key, stroke, nil, stroke)
 end
 
 -- shortcuts
