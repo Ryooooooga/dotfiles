fcf241bf662c6aa5e83ba12cd940d251cc2fa62b
Ryooooooga <eial5q265e5@gmail.com>
2022-01-12 00:34:31 +0900

    mac: add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,54 @@
+local wezterm = require('wezterm');
+
+return {
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 11.0,
+  use_ime = true,
+
+  colors = {
+    background = '#1c2022',
+    foreground = '#d8dee9',
+    ansi = {
+      '#3b4252', -- black
+      '#eb6149', -- red
+      '#73ee65', -- green
+      '#f0d448', -- yellow
+      '#81b0ff', -- blue
+      '#b48ead', -- magenta
+      '#6cd6cf', -- cyan
+      '#e5e9f0', -- white
+    },
+    brights = {
+      '#4c566a', -- black
+      '#eb6149', -- red
+      '#73ee65', -- green
+      '#f0d448', -- yellow
+      '#81b0ff', -- blue
+      '#b48ead', -- magent
+      '#6cd6cf', -- cyan
+      '#eceff4', -- white
+    },
+    tab_bar = {
+      active_tab = {
+        bg_color = "#1c2022",
+        fg_color = "#c0c0c0",
+      },
+      inactive_tab = {
+        bg_color = "#3c465a",
+        fg_color = "#808080",
+      },
+      inactive_tab_hover = {
+        bg_color = "#202426",
+        fg_color = "#909090",
+      },
+      new_tab = {
+        bg_color = "#3c465a",
+        fg_color = "#808080",
+      },
+      new_tab_hover = {
+        bg_color = "#1c2022",
+        fg_color = "#909090",
+      },
+    },
+  },
+}
