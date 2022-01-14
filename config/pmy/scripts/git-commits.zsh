dcb5bd6545afea351504069afdacca1e717f888d
Ryooooooga <eial5q265e5@gmail.com>
2022-01-14 13:22:30 +0900

    pmy: add `git-cherry-pick` completions

--- config/pmy/scripts/git-commits.zsh
+++ config/pmy/scripts/git-commits.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+args=("$@")
+
 max_count=100
 icon="\uf417"
 header="commit"
@@ -10,5 +12,6 @@ committer_date="%cr"
 git --no-pager log \
     --format="$hash%x09$subject%x09$committer_date" \
     --max-count="$max_count" \
+    "${args[@]}" \
     2>/dev/null |
     "${0:a:h}/format.zsh" "$icon" "$header" "yellow" "yellow" "gray" "dark_blue" "6" "13"
