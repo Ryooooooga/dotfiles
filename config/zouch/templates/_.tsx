4bbef6547b0f87e384c6e538f3fcbc6a1c492e73
Ryooooooga <eial5q265e5@gmail.com>
2022-03-24 21:22:04 +0900

    zouch: update tsx template

--- config/zouch/templates/_.tsx
+++ config/zouch/templates/_.tsx
@@ -1,4 +1,10 @@
+{{ if HasSuffix .Filename ".test.tsx" -}}
+{{ $component := TrimSuffix .Filename ".test.tsx" | Base | UpperCamelCase -}}
+import { render } from '@testing-library/react'
+import { {{ $component }} } from './{{ $component }}';
+{{ else -}}
 {{ $component := TrimSuffix .Filename ".tsx" | Base | UpperCamelCase -}}
 export const {{ $component }}: React.FC = () => {
   return <>{{ $component }}</>;
 };
+{{ end -}}
