fc086a3e3a0fb92e19086a400b34ee766c883db0
Ryooooooga <eial5q265e5@gmail.com>
2023-02-20 23:19:32 +0900

    zsh(qwy): improve docker completions

--- config/qwy/scripts/docker-images.zsh
+++ config/qwy/scripts/docker-images.zsh
@@ -2,5 +2,5 @@
 icon=$'\uf7c9'
 header="image"
 
-docker images --format="{{.ID}} {{.Repository}}:{{.Tag}}\t{{.Size}}\t{{.CreatedSince}}" |
+docker images --format="{{.Repository}}:{{.Tag}} {{.ID}}\t{{.Size}}\t{{.CreatedSince}}" |
     "${0:a:h}/format.zsh" "$icon" "$header" "green" "white" "dark_green" "dark_blue"
