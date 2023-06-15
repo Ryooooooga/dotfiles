a9ad16b956374df688625b2776cbacf0cee2f2bb
Ryooooooga <eial5q265e5@gmail.com>
2023-06-15 15:01:39 +0900

    zsh(qwy): update monkeywrench

--- config/qwy/scripts/npm-scripts.zsh
+++ config/qwy/scripts/npm-scripts.zsh
@@ -9,14 +9,12 @@ header_style="$ansi[yellow]"
 name_style="$ansi[bold]"
 desc_style="$ansi[dark_green]"
 
-package="$(monkeywrench node toplevel)/package.json"
-
 out_format=""
 out_format+="$header_style%s %-7s$reset  " # header
 out_format+="$name_style%-20s$reset  "     # name
 out_format+="$desc_style%s$reset\n"        # script
 
-jq -r '.scripts | to_entries | map(.key + "\t" + .value)[]' "$package" 2>/dev/null |
+monkeywrench node scripts 2>/dev/null |
     awk -F '\t' \
         -v fmt="$out_format" \
         -v icon="$icon" \
