9ceaa745ecae33d85a6b8b687f2e053b0fd07809
Ryooooooga <eial5q265e5@gmail.com>
2023-07-12 23:08:22 +0900

    zsh: disable chpwd-recent-dirs

--- config/zsh/lazy.zsh
+++ config/zsh/lazy.zsh
@@ -229,10 +229,6 @@ zinit wait lucid light-mode as'null' for \
     atload"__gdb_dashboard_atload" \
     @'cyrus-and/gdb-dashboard'
 
-### chpwd-recent-dirs ###
-add-zsh-hook chpwd chpwd_recent_dirs
-zstyle ':chpwd:*' recent-dirs-file "$XDG_STATE_HOME/chpwd-recent-dirs"
-
 ### ls-colors ###
 export LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32"
 
