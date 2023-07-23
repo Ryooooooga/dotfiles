6a9e9f8a8b9a591f96956a1745b64cc456f3673e
Ryooooooga <eial5q265e5@gmail.com>
2023-07-23 15:56:57 +0900

    zsh(qwy): remove `ansi.zsh`

--- config/qwy/scripts/format.zsh
+++ config/qwy/scripts/format.zsh
@@ -9,7 +9,21 @@ note_color="$6"
 header_width="${7:-7}"
 main_width="${8:-20}"
 
-. "${0:a:h}/ansi.zsh"
+typeset -A ansi
+ansi[reset]=$'\x1b[m'
+ansi[bold]=$'\x1b[1m'
+ansi[dim]=$'\x1b[2m'
+
+ansi[red]=$'\x1b[31m'
+ansi[green]=$'\x1b[32m'
+ansi[yellow]=$'\x1b[33m'
+ansi[blue]=$'\x1b[34m'
+ansi[magenta]=$'\x1b[35m'
+ansi[cyan]=$'\x1b[36m'
+
+ansi[gray]=$'\x1b[38;5;250m'
+ansi[dark_green]=$'\x1b[38;5;65m'
+ansi[dark_blue]="$ansi[dim]$ansi[blue]"
 
 reset="$ansi[reset]"
 header_style="$ansi[$header_color]"
