dd72440ca4945a2f4f6c3432a70d1f067e0a4f70
Ryooooooga <eial5q265e5@gmail.com>
2023-09-21 22:37:16 +0900

    zsh(gdb): remove gdb-dashboard

--- config/zsh/lazy.zsh
+++ config/zsh/lazy.zsh
@@ -1,6 +1,7 @@
 ### Aliases ###
 alias la='ls -a'
 alias ll='ls -al'
+alias gdb='gdb -q -nh'
 
 case "$OSTYPE" in
     linux*)
@@ -230,15 +231,6 @@ zinit wait lucid light-mode as'program' from'gh-r' for \
     atclone'__mdmg_atclone' atpull'%atclone' \
     pick'mdmg*/mdmg' \
     @'himanoa/mdmg'
-
-### GDB ###
-__gdb_dashboard_atload() {
-    export GDBHISTFILE="$XDG_STATE_HOME/gdb_history"
-    alias gdb="gdb -q -nh -x '$PWD/.gdbinit'"
-}
-zinit wait lucid light-mode as'null' for \
-    atload"__gdb_dashboard_atload" \
-    @'cyrus-and/gdb-dashboard'
 
 ### ls-colors ###
 export LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32"
