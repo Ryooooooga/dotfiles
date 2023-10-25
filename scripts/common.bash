fdf015bfec144c018cc07798d875690015fbf306
Ryooooooga <eial5q265e5@gmail.com>
2023-10-25 20:05:00 +0900

    scripts: fix duplicated code

--- scripts/common.bash
+++ scripts/common.bash
@@ -10,4 +10,3 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 
 export DENO_INSTALL="${DENO_INSTALL:-$XDG_DATA_HOME/deno}"
-export DENO_INSTALL="${DENO_INSTALL:-$XDG_DATA_HOME/deno}"
