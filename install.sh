82e6cb436ff2475490add890587183229a157c8c
Ryooooooga <Ryooooooga@users.noreply.github.com>
2024-07-06 16:13:26 +0000

    deploy: e970551ea40eb75bea52f6ca9f88eee29ae0c0d9

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 INSTALL_DIR="${INSTALL_DIR:-$HOME/repos/github.com/Ryooooooga/dotfiles}"
 
 if [ -d "$INSTALL_DIR" ]; then
-    echo "Updating dotfiles..."
-    git -C "$INSTALL_DIR" pull
+  echo "Updating dotfiles..."
+  git -C "$INSTALL_DIR" pull
 else
-    echo "Installing dotfiles..."
-    git clone https://github.com/Ryooooooga/dotfiles "$INSTALL_DIR"
+  echo "Installing dotfiles..."
+  git clone https://github.com/Ryooooooga/dotfiles "$INSTALL_DIR"
 fi
 
 /bin/bash "$INSTALL_DIR/scripts/setup.bash"
