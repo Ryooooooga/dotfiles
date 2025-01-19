d8bf39527a21fe92852974bd639e2f2dff88199a
Ryooooooga <eial5q265e5@gmail.com>
2025-01-19 20:20:52 +0900

    karabiner: add mk24 keyboard

--- config/karabiner/karabiner.ts
+++ config/karabiner/karabiner.ts
@@ -30,26 +30,32 @@ const DEVICES = {
     vendor_id: 1452,
   }),
   macBook2018: keyboard({
-    product_id: 634,
     vendor_id: 1452,
+    product_id: 634,
   }),
   progresTouchRetroTiny: keyboard({
-    product_id: 4,
     vendor_id: 11240,
+    product_id: 4,
   }),
   realforceR2: keyboard({
-    product_id: 328,
     vendor_id: 2131,
+    product_id: 328,
   }),
   akl680: keyboard({
-    product_id: 591,
     vendor_id: 1452,
+    product_id: 591,
   }),
   mk23: (is_pointing_device: boolean = true) =>
     keyboard({
       is_pointing_device: is_pointing_device || undefined,
-      product_id: 41888,
       vendor_id: 41606,
+      product_id: 41888,
+    }),
+  mk24: (is_pointing_device: boolean = true) =>
+    keyboard({
+      is_pointing_device: is_pointing_device || undefined,
+      vendor_id: 41606,
+      product_id: 44451,
     }),
 } as const;
 
@@ -412,6 +418,14 @@ const profile = defaultProfile({
     },
     {
       identifiers: DEVICES.mk23(false),
+      ignore: true,
+    },
+    {
+      identifiers: DEVICES.mk24(),
+      ignore: false,
+    },
+    {
+      identifiers: DEVICES.mk24(false),
       ignore: true,
     },
   ],
