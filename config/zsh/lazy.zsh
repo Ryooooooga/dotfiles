74585cd568fefb4b25f99277fff7554c417de08e
Ryooooooga <eial5q265e5@gmail.com>
2026-01-26 21:49:59 +0900

    npm: remove global npmrc

--- config/zsh/lazy.zsh
+++ config/zsh/lazy.zsh
@@ -134,12 +134,6 @@ export LESSHISTFILE='-'
 ### Node.js ###
 export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_history"
 
-### npm ###
-export NPM_CONFIG_DIR="$XDG_CONFIG_HOME/npm"
-export NPM_DATA_DIR="$XDG_DATA_HOME/npm"
-export NPM_CACHE_DIR="$XDG_CACHE_HOME/npm"
-export NPM_CONFIG_USERCONFIG="$NPM_CONFIG_DIR/npmrc"
-
 ### irb ###
 export IRBRC="$XDG_CONFIG_HOME/irb/irbrc"
 
