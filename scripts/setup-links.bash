aa64ffd1aa0cf045c2f2616ffbcc35da11e9c04a
Ryooooooga <eial5q265e5@gmail.com>
2026-06-18 19:55:56 +0900

    claude: update global config

--- scripts/setup-links.bash
+++ scripts/setup-links.bash
@@ -20,5 +20,6 @@ mkdir -p \
 
 ln -sfv "$REPO_DIR/config/"* "$XDG_CONFIG_HOME"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
+ln -sfv "$XDG_CONFIG_HOME/claude" "$HOME/.claude"
 ln -sfv "$XDG_CONFIG_HOME/editorconfig/.editorconfig" "$HOME/.editorconfig"
 ln -sfnv "$XDG_CONFIG_HOME/vim" "$HOME/.vim"
